337abe971cd7ac05aca20f74659dea5ca9890ac4
Sunil kumar <sunilkivdor@gmail.com>
2025-11-14 01:44:32 +0530

    server & router code

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,48 +1,19 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"os"
-	"os/exec"
+	"github.com/SunilKividor/donela/internal/api"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	engine := gin.New()
+	engine.Use(gin.Logger())
+	server := api.NewServer(engine, "3000")
 
-	err := os.MkdirAll("transcoded", os.ModePerm)
-	if err != nil {
-		fmt.Println("Error creating folder")
-		return
-	}
+	api.RegisterRoutes(engine)
 
-	// cmd := exec.Command(
-	// 	"ffprobe",
-	// 	"-v", "error",
-	// 	"-show_format",
-	// 	"-show_streams",
-	// 	"-print_format", "json",
-	// 	"transcoded/Flash_in_the_Pan.webm",
-	// )
-
-	cmd := exec.Command(
-		"ffmpeg", "-i",
-		"flac_audio_samples/Flash_in_the_Pan.flac",
-		"-vn",
-		"-c:a", "libopus",
-		"-b:a", "128k",
-		"-ar", "48000",
-		"-ac", "2",
-		"transcoded/Flash_in_the_Pan.webm",
-	)
-
-	var stderr bytes.Buffer
-	cmd.Stderr = &stderr
-	err = cmd.Run()
-
-	if err != nil {
-		fmt.Println("Error running ffprobe:", err)
-		fmt.Println("ffproble error: ", stderr.String())
-		return
+	if err := server.Serve(); err != nil {
+		panic(err)
 	}
 
 }
